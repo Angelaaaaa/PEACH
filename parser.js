75d314d9cd16acbca58a2cc65fa82c9b58abab74
Your Name <mik.german@gmail.com>
2016-12-11 21:51:30 +0000

    Working parser. Tested on 1 archetype

--- parser.js
+++ parser.js
@@ -1,25 +1,43 @@
 function uploadXML() {
 
-  var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
 
-  request.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      callback(this);
-    }
-  };
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            parser(this);
+        }
+    };
 
-  request.open("GET", "sample_template.xml", true);
-  request.send();
+    request.open("GET", "sample_template.xml", true);
+    request.send();
 }
 
-function callback(xml) {
-  var doc, arch, arch_no;
-  doc = xml.responseXML;
-  
-  arch = doc.getElementsByTagName("archetype");  //get archetypes
+function parser(xml) {
+    var doc, arch;
+    doc = xml.responseXML;
 
-  arch_no = arch.length;
-  
+    arch = doc.getElementsByTagName("archetype"); //get archetypes
+
+
+    for (i = 0; i < arch.length; i++) { //number of archetypes in the teamplate
+
+        //name of an archetype
+        console.log(arch[i].getElementsByTagName("name")[0].textContent);
+
+        //collection of elements contained in one archetype
+        var elements = arch[i].getElementsByTagName("element");
+
+        
+
+        for (i = 0; i < elements.length; i++) { //for every element
+
+          console.log(elements[i].getElementsByTagName("name")[0].textContent);
+
+          console.log(elements[i].getElementsByTagName("type")[0].textContent);
+        }
+
+      
+    }
 
 }
 
