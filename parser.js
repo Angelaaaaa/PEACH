8c503ff1085df0a7ae4e13613f104f7b5a11afc5
Your Name <mik.german@gmail.com>
2016-12-11 22:04:29 +0000

    Working dynamic version for multiple archetypes and elements

--- parser.js
+++ parser.js
@@ -13,30 +13,36 @@ function uploadXML() {
 }
 
 function parser(xml) {
-    var doc, arch;
+    var doc, arch, i, j, elements, name_arch, name_elem, type;
     doc = xml.responseXML;
 
     arch = doc.getElementsByTagName("archetype"); //get archetypes
 
-
     for (i = 0; i < arch.length; i++) { //number of archetypes in the teamplate
 
         //name of an archetype
-        console.log(arch[i].getElementsByTagName("name")[0].textContent);
+        name_arch = arch[i].getElementsByTagName("name")[0].textContent;
+        console.log(name_arch);
 
         //collection of elements contained in one archetype
-        var elements = arch[i].getElementsByTagName("element");
+        elements = arch[i].getElementsByTagName("element");
 
         
 
-        for (i = 0; i < elements.length; i++) { //for every element
+        for (j = 0; j < elements.length; j++) { //for every element
 
-          console.log(elements[i].getElementsByTagName("name")[0].textContent);
+          //name and type of each element within an archetype
+          name_elem = elements[j].getElementsByTagName("name")[0].textContent;
+          type = elements[j].getElementsByTagName("type")[0].textContent;
 
-          console.log(elements[i].getElementsByTagName("type")[0].textContent);
+          console.log('***');
+          console.log(name_elem);
+          console.log(type);
+          console.log('***');
         }
 
-      
+
+      console.log('---');
     }
 
 }
