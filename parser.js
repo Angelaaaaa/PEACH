2223cf5c815141a070680223aa59457d4cae9af4
German Mikulski <German Mikulski>
2016-12-11 20:29:23 +0000

    Pre-server parsing of a random tag

--- parser.js
+++ parser.js
@@ -1,1 +1,24 @@
-var parser;
+function uploadXML() {
+
+  var request = new XMLHttpRequest();
+
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      callback(this);
+    }
+  };
+
+  xmlhttp.open("GET", "sample_template.xml", true);
+  xmlhttp.send();
+}
+
+function callback(xml) {
+  var doc;
+  doc = xml.responseXML;
+  
+  x = doc.getElementsByTagName("rand")[0].childNodes[0].nodeValue;
+  console.log(x);
+   
+}
+
+uploadXML();
